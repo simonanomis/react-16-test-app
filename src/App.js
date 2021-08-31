56f53dd69fd7ae98a9aeac7b60c53243eb7569c7
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-08-31 14:02:06 +0200

    Manipulation the state, func-stateless vs class-stateful comp, pass method ref between comp

--- src/App.js
+++ src/App.js
@@ -12,8 +12,16 @@ class App extends Component{
         ]
     }
 
-    switchNameHandler = () => {
-        console.log('Was clicked');
+    switchNameHandler = (newName) => {
+        //console.log('Was clicked');
+        // DONT DO THIS: this.state.persons[0].name = 'Maximilian';
+        this.setState({
+            persons: [
+                {name: newName, age: 40},
+                {name: 'Simona', age: 35},
+                {name: 'Filip', age: 33}
+            ]
+        });
     }
 
     render() {
@@ -21,10 +29,17 @@ class App extends Component{
             <div className="App">
                 <h1>Hi, I am Simona</h1>
                 <p>This is working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My Hobbies are LOL</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age} />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'Maximilian!!')}> My Hobbies are LOL</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
             </div>
         );
     }

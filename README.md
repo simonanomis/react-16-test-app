708d026765f8288ee6a958e413f3ba82b28bb2be
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-09-01 12:06:37 +0200

    add style css and inline styling

--- README.md
+++ README.md
@@ -70,3 +70,6 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+![img_2.png](img_2.png)
--- src/App.js
+++ src/App.js
@@ -35,11 +35,21 @@ class App extends Component{
     }
 
     render() {
+        const style = {
+          backgroundColor: 'white',
+          font: 'inherit',
+          border: '1px solid blue',
+          padding: '8px',
+            cursor: 'pointer',
+        };
+
         return (
             <div className="App">
                 <h1>Hi, I am Simona</h1>
                 <p>This is working!</p>
-                <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+                <button
+                    style={style}
+                    onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age}
